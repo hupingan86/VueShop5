a9ccb5ff4456869630e435089bac3fcd40af5180
hupingan86 <406839815@qq.com>
2019-07-31 17:37:59 +0800

    settinngs

--- VueShop/settings.py
+++ VueShop/settings.py
@@ -150,6 +150,10 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = "/media/"
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static")
+)
+
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 REST_FRAMEWORK = {
